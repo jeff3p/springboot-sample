b5883c0269bf977fce0e4a6030766827c825ed0b
jeff3p <jeff3p@gmail.com>
2026-01-02 09:00:09 -0500

    Create DemoApplication.java

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
